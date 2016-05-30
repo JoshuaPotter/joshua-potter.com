1ac047781a1babeb58b310115597fa287b51b6ff
Joshua Potter <tljoshh@gmail.com>
2016-05-29 21:21:39 -0400

    Commit

---
+++ clients/vijay/betterfinances/index.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<!-- saved from url=(0511)http://betterfinances.co/hi/insurance/rcaltft/index-t2.php?region=Massachusetts&voluumdata=vid..00000003-9642-42d1-8000-000000000000__vpid..7ba82800-07e2-11e6-80d3-a44018a47dd8__caid..85edb041-4034-4c28-9458-f5d7cc5a6520__rt..R__lid..87d5ec42-df58-4422-9a31-5b986a87373b__oid1..1fae7e91-cf0d-4ab3-aa0b-4dc02154c900__var1..359382__var2..47968__var3..conservative%20news__var4..30__rd..conservativetribune%5C.%5Ccom__aid..__ab..__sid..&content_id=359382&boost_id=47968&adv_targets=conservative%20news&widget_id=30 -->
+<html lang="en">
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="pinterest" content="nopin">
+	<title>Better Finances</title>
+	<link href="./index_files/bootstrap.min.css" rel="stylesheet">
+	<link href="./index_files/styles.css" rel="stylesheet">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+<style type="text/css">.navbar-nav {display:none}</style>
+<script src="files/html5shiv.min.js"></script>
+<script src="files/respond.min.js"></script>
+<![endif]-->
+</head>
+
+
+<body cz-shortcut-listen="true">
+
+	<div class="header">
+		<div class="container">
+			<a href="click.php" target="_blank" rel="nofollow" class="logo">
+				<img src="./index_files/logo.png"></a>
+			<ul class="top-menu hidden-xs">
+				<li><a href="click.php" target="_blank" rel="nofollow">Home</a></li>
+				<li><a href="click.php" target="_blank" rel="nofollow">Mortgage</a></li>
+				<li><a href="click.php" target="_blank" rel="nofollow">Debt Relief</a></li>
+				<li><a href="click.php" target="_blank" rel="nofollow" class="select">Insurance</a></li>
+				<li><a href="click.php" target="_blank" rel="nofollow">Credit Score</a></li>
+			</ul>
+		</div>
+	</div>
+
+	<div class="man-content">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8">
+					<h1>New Rule Leaves Massachusetts Drivers Furious and Shocked!</h1>
+					<h3>Do not pay your next car insurance bill until you read this...</h3>
+					<p class="date-text">
+						<script language="Javascript">
+							var monthNames = new Array(
+								"January", "February", "March", "April", "May", "June", "July",
+								"August", "September", "October", "November", "December");
+
+							var now = new Date();
+							document.write(monthNames[now.getMonth()] + " " + now.getDate() + ", " + now.getFullYear());
+
+							// -->
+						</script>May 26, 2016</p>
+					<a href="click.php" target="_blank">
+						<img src="./index_files/MA.jpg" class="content-img img-responsive">
+					</a>
+					<p>Massachusetts — New policies are indicating that for years many Massachusetts drivers have likely overpaid on their car insurance.
+						<br><br> This is the 1 simple truth your car insurance company doesn't want you to know. If you <strong>drive less than 50 miles/day and live in a qualified zip code</strong> you can get an extremely high discount. Additionally, if you have no DUI's,
+						you can get even more discounts. But do you think your auto insurance company will tell you that?
+						<br><br> When drivers enter their zip code and driver information at <a href="click.php" target="_blank">Provide Savings™ official site</a> many are shocked at the results they find. Most just cannot believe that the available
+						rates are in fact real, but the truth is rates have dropped significantly over the past 12 months and thanks to new program policies it's now easy to <strong>save up to 50% with rates as low as $29/month</strong>.
+						<br><br> Nikki Johnson, the authority on everything related to insurance, set out to do some research and determine whether these types of services live up to their reputations. After several weeks Nikki was able to report back on her findings,
+						with her most exciting of which being that she is now going to save $296.28 on her own insurance premiums over the next year, and there are many other people who have done the same and better.
+						<br><br>
+						<strong>So, what is the "One New Rule?"</strong> NEVER buy insurance without <a href="click.php" target="_blank">comparing all of the discounts online</a> first. </p>
+					<div class="text-center" style="color: #5c6270; margin-bottom:10px;"><em>example savings</em></div>
+					<div class="row">
+						<div class="col-md-6" style="margin-bottom:30px;">
+							<img src="./index_files/old-bill.gif" class="img-responsive center-block">
+						</div>
+						<div class="col-md-6" style="margin-bottom:30px;">
+							<img src="./index_files/new-bill.gif" class="img-responsive center-block">
+						</div>
+					</div>
+					<p>Does this mean Nikki was being overcharged by her former insurance agent? She would not say one way or the other, but the truth is most agents are paid on commission which means the higher your insurance premiums, the more money your agent makes.
+						You can be sure, however, that your insurance company is not going to call you up to offer you a discount.
+					</p>
+					<blockquote>
+						<p>Note: You're NEVER LOCKED into your current policy. If you've already paid your current policy, you can very easily cancel and the balance will be refunded.
+						</p>
+					</blockquote>
+					<p>
+						With average savings of 32%, these online services are gaining massive popularity. One of the most trusted, secure and effective free online service that provides drivers with lower insurance rates is <a href="click.php"
+							target="_blank">Provide Savings™</a>. Over 1,000,000 U.S. drivers have already trusted Provide Savings to get significant discounts.
+					</p>
+					<h2>Here's How You Do It:</h2>
+					<p>
+						<strong>Step 1:</strong> <a href="click.php" target="_blank">Click
+                 your state on the map to instantly check your zip code for free</a>.
+					</p>
+					<p><strong>Step 2:</strong> Once you go through a few questions, you will have the opportunity to compare the best carrier quotes in your area for an average savings of 32% a month.
+					</p>
+					<div class="map">
+						<a href="click.php" target="_blank" rel="nofollow">
+							<img src="./index_files/map.gif" class="img-responsive">
+						</a>
+					</div>
+					<p>
+						<a class="btn btn-primary" href="click.php" target="_blank" rel="nofollow">
+                    Get Your New Rate - FREE »
+                 </a>
+					</p>
+				</div>
+				<div class="col-md-4">
+					<a href="click.php" target="_blank" rel="nofollow">
+						<img class="img-responsive" src="./index_files/selectcars.gif">
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="footer">
+		<div class="container">
+			<p>THIS PAGE RECEIVES COMPENSATION FROM SERVICES AND PRODUCTS FEATURED ON THIS SITE. This site is in no way affiliated with any news source. This site contains affiliate and partner links. This website and the company that owns it is not responsible for
+				any typographical or photographic errors. If you do not agree to our terms and policies, then please leave this site immediately. *Clarification of the advertising headline "New Rule in Massachusetts". - Merriam-Webster's Dictionary defines the word
+				"rule" as "a piece of advice about the best way to do something" (http://www.merriam-webster.com/dictionary/rule). This article aims to advise the public that comparing rates is one of the best ways that you can save money on car insurance. No matter
+				what city, state or zip code you live in you can compare rates and get free quotes. If you truly want to find the best rate and save on car insurance then follow our advice or "rule" to compare rates. <a href="http://www.bankrate.com/finance/insurance/car-insurance-why-do-people-switch.aspx"
+					target="_blank">Here</a> is a link to a survey which demonstrate the importance of comparing rates and how applying this "rule", in any state city or zip code, can help drivers save 32% and $368 per year. A <a href="http://www.nerdwallet.com/blog/insurance/2013/11/01/drivers-overpay-368-car-insurance-year/"
+					target="_blank">second survey</a> which analyzed car insurance quotes for 1,000 zip codes across the U.S. found that within a given zip code, rates vary by 154% on average, allowing drivers to find an average of 32% and $368 per year in savings.
+
+
+			</p>
+			<hr>
+			<p>© 2015 BetterFinances. All Rights Reserved.
+				<a href="http://smartlifestyletrends.com/privacy-policy-slt.pdf " target="_blank">Privacy Policy</a>
+				<a href="http://smartlifestyletrends.com/terms-of-service/" target="_blank">Terms of Use</a>
+			</p>
+		</div>
+	</div>
+</body>
+
+</html>
--- dev/portfolio3/index.php
+++ dev/portfolio3/index.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Last.FM API: 
+	Last.FM API: 
 	Application name	joshua-potter.com
 	API key	b935fbba15b18f756a2c012e19bbae31
 	Shared secret	3b4ea08aec8241d8a60f6cf4b83113c5
@@ -78,57 +78,7 @@ Last.FM API:
 					</div>
 					<div class="row">
 						<div class="col-md-12">
-							<div id="spotify">
-								<div id="cover">
-									<div class="row">
-										<a class="album">
-											<img src="" id="album-cover" alt="Album cover" class="img-responsive" crossOrigin='' />
-											<div class="record">
-											<div class="eye"></div>
-											  <svg class="disc" width="150px" height="150px" viewBox="0 0 150 150" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:sketch="http://www.bohemiancoding.com/sketch/ns">
-												<defs></defs>
-												<g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" sketch:type="MSPage">
-													<g id="Group" sketch:type="MSLayerGroup" transform="translate(75.000000, 75.000000) scale(1, -1) translate(-75.000000, -75.000000) ">
-														<path d="M75,150 C116.421356,150 150,116.421356 150,75 C150,33.5786438 116.421356,0 75,0 C33.5786438,0 0,33.5786438 0,75 C0,116.421356 33.5786438,150 75,150 Z" id="Oval-1" fill="#312F2D" sketch:type="MSShapeGroup"></path>
-														<path d="M27.7421875,75 C27.7421875,101.017578 51.7402344,122.183594 75,122.183594" id="Path-1" stroke="#5A5A5A" stroke-width="2" stroke-linecap="round" sketch:type="MSShapeGroup"></path>
-														<path d="M75,38.1484375 C75,58.4688144 93.4978363,75 111.426601,75" id="Path-1-Copy-3" stroke="#5A5A5A" stroke-width="2" stroke-linecap="round" sketch:type="MSShapeGroup" transform="translate(93.213301, 56.574219) scale(-1, -1) translate(-93.213301, -56.574219) "></path>
-														<path d="M38.0904708,75 C38.0904708,95.3203769 56.8335434,111.851562 75,111.851562" id="Path-1-Copy" stroke="#5A5A5A" stroke-width="2" stroke-linecap="round" sketch:type="MSShapeGroup"></path>
-														<path d="M75,27.8164062 C75,53.8339844 98.9980469,75 122.257812,75" id="Path-1-Copy-4" stroke="#5A5A5A" stroke-width="2" stroke-linecap="round" sketch:type="MSShapeGroup" transform="translate(98.628906, 51.408203) scale(-1, -1) translate(-98.628906, -51.408203) "></path>
-													</g>
-												</g>
-											</svg>
-											</div>
-										</a>
-										<div class="progress">
-											<div class="active"></div>
-										</div>
-										<div class="col-md-3 valign">
-											<div class="animation">
-												<div class="waveform"></div>
-												<div class="waveform"></div>
-												<div class="waveform"></div>
-												<div class="waveform"></div>
-											</div>
-										</div>
-										<div class="col-md-7 col-sm-offset-1 valign">
-											<div class="song">
-												<div class="track"></div>
-												<div class="artist"></div>
-											</div>
-										</div>
-									</div>
-								</div>
-								<a href="https://open.spotify.com/user/tljoshh" target="_blank" id="feed" title="Follow me on Spotify">
-									<!-- 							<a href="https://open.spotify.com/user/tljoshh" target="_blank" id="spotify" class="popup slow" data-title="Follow me on Spotify"> -->
-									<span>I'm currently listening to </span>
-									<strong></strong><span class="track"></span><strong></strong> 
-									<span>by</span>
-									<span class="artist"></span>
-									<span>on</span>
-									<i class="fa fa-spotify fa-2x" aria-hidden="true"></i>
-									<span class="spotify">Spotify</span>
-								</a>
-							</div>
+							<div id="spotifyJs"></div>
 						</div>
 					</div>
 				</div>
@@ -138,170 +88,19 @@ Last.FM API:
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
 		<script src="js/colorthief.js"></script>
+		<script src="js/spotify.js"></script>
 		<script>
 			$(document).ready(function() {
-				var progress,
-					audioObject = null;
-				function spotify(user, key) {
-					$.ajax({
-							url: "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=" + user + "&api_key=" + key + "&limit=1&format=json",
-							type: "GET",
-							dataType: "json",
-						})
-						.always(function() {
-							console.log('Checking Last.fm for updates');
-						})
-						.done(function(json) {
-							//console.log(json);
-							// if currently listening, search spotify for track details with last.fm response
-							//   and update music card with metadata
-							try {
-								if (json.recenttracks.track[0]["@attr"].nowplaying) {
-									if(json.recenttracks.track[0].name.toLowerCase() !== $('#spotify #feed .track').text().toLowerCase()) {
-										$('#spotify').addClass('active');
-										console.log('Update received from  Last.fm');
-										// get spotify metadata based on last.fm track info
-										$.ajax({
-											url: "https://api.spotify.com/v1/search?q=" + "track:" + encodeURIComponent(json.recenttracks.track[0].name) + " artist:" + encodeURIComponent(json.recenttracks.track[0].artist['#text']) + " album:" + encodeURIComponent(json.recenttracks.track[0].album['#text']) +  "&type=track&limit=1",
-											type: "GET",
-											dataType: "json",
-										})
-										.always(function() {
-											console.log('Getting metadata from Spotify');
-											
-											// reset timestamp
-											clearInterval(progress);
-											$('#spotify #cover .progress .active').animate({
-												'width': $('#spotify #cover').width() * 0.01
-											}, 10);
-										})
-										.done(function(json) {
-											console.log(json);
-											// format and update artist names
-											var artistLinks = "";
-											var artists = "";
-												if(json.tracks.items[0].artists.length > 1) {
-													$.each(json.tracks.items[0].artists, function(i,v) {
-														artists += json.tracks.items[0].artists[i].name + ", ";
-														artistLinks += "<a href='" + json.tracks.items[0].artists[i].uri + "'>" + json.tracks.items[0].artists[i].name + "</a>, ";
-													$('#spotify #cover .artist').attr("data-artist-" + i + "-id", json.tracks.items[0].artists[i].id);
-													});
-												} else {
-													artists = json.tracks.items[0].artists[0].name;
-													artistLinks = "<a href='" + json.tracks.items[0].artists[0].uri + "'>" + json.tracks.items[0].artists[0].name + "</a>";
-												$('#spotify #cover .artist').attr("data-artist-0-id", json.tracks.items[0].artists[0].id);
-												}
-											// peel off trailing comma
-											if(json.tracks.items[0].artists.length > 1) {
-												artists = artists.slice(0, -2);
-												artistLinks = artistLinks.slice(0, -2);
-											}
-											$('#spotify #cover .artist').html(artistLinks);
-											$('#spotify #feed .artist').html(json.tracks.items[0].artists[0].name);
-											
-											// format and update track name
-											if (json.tracks.items[0].name.length > 27) {
-												var str = json.tracks.items[0].name.substring(0, 24) + "...";
-												$('#spotify #cover .track').html("<a href='" + json.tracks.items[0].uri + "'>" + str + "</a>");
-												$('#spotify #feed .track').html(json.tracks.items[0].name);
-											} else {
-												$('#spotify #cover .track').html("<a href='" + json.tracks.items[0].uri + "'>" + json.tracks.items[0].name + "</a>");
-												$('#spotify #feed .track').html(json.tracks.items[0].name);
-											}
-											$('#spotify #cover .track').attr('data-track-id', json.tracks.items[0].id);
-										
-											// update album cover
-											if(json.tracks.items[0].album.images[1].url.length > 0) {
-												$('#cover .album').attr('href', json.tracks.items[0].preview_url).attr("data-album-id", json.tracks.items[0].album.id);
-												$('#cover .album img').attr('src', json.tracks.items[0].album.images[1].url).attr('title', json.tracks.items[0].album.name);
-											}
-											
-											// update waveform colors based on new album cover
-											$('#album-cover').on('load', function() {
-												var sourceImage = document.getElementById("album-cover");
-												var colorThief = new ColorThief();
-												var color = colorThief.getPalette(sourceImage,4);
-												$('#spotify #cover .progress .active').css("background", "rgb(" + color[3] + ")");
-												$('.waveform:nth-of-type(1)').css("background", "rgb(" + color[3] + ")");
-												$('.waveform:nth-of-type(2)').css("background", "rgb(" + color[1] + ")");
-												$('.waveform:nth-of-type(3)').css("background", "rgb(" + color[0] + ")");
-												$('.waveform:nth-of-type(4)').css("background", "rgb(" + color[2] + ")");
-												//$('#spotify .album .record #Oval-1').css("fill", "rgb(" + color[1] + ")");
-												$('#spotify .album .record .eye').css("background", "rgb(" + color[3] + ")");
-											});
-											
-											// start timestamp
-											// this is only an estimate, as last.fm and spotify do not 
-											//   provide access to how much of a track you have listened to
-											// adds ~1% in timestamp progress every 1% of the song listened to
-											//   but is entirely dependent upon update rate of the api, causing
-											//   up to 15 seconds delay initially
-											var duration = json.tracks.items[0].duration_ms;
-											var progressInterval = (duration * 0.015); // find equivalent of 1% of duration in milliseconds
-											var listenedTo = ($('#spotify #cover').width() * 0.015);
-											progress = setInterval(function() {
-												$('#spotify #cover .progress .active').animate({
-													'width': "+=" + listenedTo
-												},(progressInterval/2), "linear");
-											}, progressInterval);
-										});
-									}
-								}
-							} catch (e) {
-								clearInterval(progress);
-								$('#spotify').removeClass('active');
-							}
-						});
-				}
-
-				function init() {
-					// Spotify module
-					var lastFmUsername = 'TLJoshh';
-					var lastFmKey = 'b935fbba15b18f756a2c012e19bbae31';
-					var lastFmInterval = 15000;
-					spotify(lastFmUsername, lastFmKey);
-					setInterval(function() {
-						if($('#spotify').hasClass('active')) {
-							lastFmInterval = 15000;
-						} else {
-							lastFmInterval = 60000;
-						}
-						spotify(lastFmUsername, lastFmKey);
-					}, lastFmInterval);
-					$('#spotify #cover .album').click(function (e) {
-						e.preventDefault();
-						var parent = $('#spotify');
-						var target = $(this);
-						if (target !== null) {
-							if (parent.hasClass('playing')) {
-								audioObject.pause();
-							} else {
-								if (audioObject) {
-									audioObject.pause();
-								}
-								audioObject = new Audio(target.attr('href'));
-								audioObject.play();
-								parent.addClass('playing');
-								audioObject.addEventListener('ended', function () {
-									parent.removeClass('playing');
-								});
-								audioObject.addEventListener('pause', function () {
-									parent.removeClass('playing');
-								});
-							}
-						}
-					});
-				}
-				
-				init();
-				
+				var lastFmUsername = 'TLJoshh';
+				var lastFmKey = 'b935fbba15b18f756a2c012e19bbae31';
+				spotifyJs(lastFmUsername, lastFmKey, lastFmUsername, false);
 				$('.navigation-interaction').click(function() {
 					var navPane = $('#navigation');
 					var pagePane = $('#page');
 					if ($(window).width() > 1680) {
 						if (navPane.hasClass('open')) {
 							pagePane.animate({
-								marginLeft: "105px"
+								marginLeft: "90px"
 							}, 1450, "easeOutCubic");
 							navPane.animate({
 								width: "105px"
@@ -317,7 +116,7 @@ Last.FM API:
 					} else {
 						if (navPane.hasClass('open')) {
 							pagePane.animate({
-								marginLeft: "105px"
+								marginLeft: "90px"
 							}, 1450, "easeOutCubic");
 							navPane.animate({
 								width: "105px"
